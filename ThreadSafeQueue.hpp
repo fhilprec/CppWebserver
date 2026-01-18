c4c2c240381b1a7af0500a0324bb0df6f3f7d9a0
florian <fhilprec@gmail.com>
2026-01-18 15:56:47 +0100

    webbrowser access working

---
+++ ThreadSafeQueue.hpp
@@ -0,0 +1,33 @@
+#include <mutex>
+#include <queue>
+
+
+
+
+class ThreadSafeQueue {
+    public:
+        ThreadSafeQueue() = default;
+
+        void push(int value) {
+            std::lock_guard<std::mutex> lock(mutex);
+            queue_.push(value);
+        }
+
+        int pop() {
+            std::lock_guard<std::mutex> lock(mutex);
+            if (!queue_.empty()) {
+                int value = queue_.front();
+                queue_.pop();
+                return value;
+            }
+            return -1; // Indicates empty queue
+        }
+
+        bool isEmpty() {
+            std::lock_guard<std::mutex> lock(mutex);
+            return queue_.empty();
+        }
+    private:
+        std::queue<int> queue_;
+        std::mutex mutex;
+};
